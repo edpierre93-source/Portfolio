4ef24f880b77b989cbfcfb24d2f00bea5226c553
edpierre93-source <edpierre93@gmail.com>
2026-09-03 16:03:19 +0200

    Revert "fix esLint"
    This reverts commit 39af454a6d95b9cf23330d93c80a33a6921f508b.

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,16 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
+
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
 ]);
+
+import { config } from 'vite';
+import eslint from 'vite-plugin-eslint2';
+
+export default config({
+  plugins: [
+    eslint() // Ajoute ESLint au processus de build/dev de Vite
+  ],
+});
